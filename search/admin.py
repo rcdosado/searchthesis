2442dfdbc5b3bf278fcc8d6940d73bcc486218b9
Roy Cyril Dosado <rcdosado@gmail.com>
2018-12-30 18:40:37 +0800

    first commit

---
+++ search/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from search.models import Thesis 
+
+@admin.register(Post)
+class ThesisAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'authors','posted_by','publish',
+                    'status')
+    list_filter = ('status', 'created', 'publish', 'posted_by')
+    search_fields = ('title', 'abstract')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('posted_by',)
+    date_hierarchy = 'publish'

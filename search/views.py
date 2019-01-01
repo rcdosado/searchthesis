5f89982d08856f00482047e18f6f9e34e7930d54
Roy Cyril Dosado <rcdosado@gmail.com>
2019-01-01 12:17:42 +0800

    make search functional

--- search/views.py
+++ search/views.py
@@ -1,11 +1,22 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+
+
 from searchthesis.settings import PAGINATION_PER_PAGE
 from .models import Thesis 
 
 
 def thesis_list(request):
     object_list = Thesis.published.all()
+    if request.method == 'POST':
+        query = request.POST.get("q")
+        if query:
+            object_list=Thesis.objects.filter(
+                Q(title__icontains=query) |
+                Q(abstract__icontains=query) |
+                Q(authors__icontains=query) 
+                ).order_by('-publish')
     paginator = Paginator(object_list, PAGINATION_PER_PAGE)
     page = request.GET.get('page')
     try:
